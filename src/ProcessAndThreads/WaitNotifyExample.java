9ba242773947e10732db6d540c8a1cbe6660ddc4
osanfelix <osanfelix@outlook.es>
2017-01-24 16:03:39 +0100

    Añadido un ejemplo en elq eu se usa wait() y notify().
    Llamar desde el main(...) a WaitNotifyExample.test():
    - Esto imprimirá "Hola Adeu Hola Adeu..." 5 veces y siempre por ese orden

---
+++ src/ProcessAndThreads/WaitNotifyExample.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProcessAndThreads;
+
+/**
+ *
+ * @author Ofelia
+ */
+public class WaitNotifyExample
+{
+    
+    public static void test() {
+        escriuHolaAdeu eha = new escriuHolaAdeu();
+        E_Adeu e_Adeu = new E_Adeu(eha);
+        E_Hola e_Hola = new E_Hola(eha);
+    }
+}
+ 
+class E_Hola implements Runnable {
+    escriuHolaAdeu eh;
+    E_Hola (escriuHolaAdeu eh)
+    {
+        this.eh = eh;
+        new Thread(this, "Hola").start();
+    }
+
+    public void run() {
+        try{
+            for(int x=0;x<5;x++) {
+                Thread.sleep(1000);
+                eh.eHola();
+            }
+        }catch (InterruptedException e){}
+    }
+}
+ 
+class E_Adeu implements Runnable {
+    escriuHolaAdeu eh;
+    E_Adeu (escriuHolaAdeu eh) {
+        this.eh = eh;
+        new Thread(this, "Adéu").start();
+    }
+    public void run() {
+        for(int x=0;x<5;x++) {
+            eh.eAdeu();
+        }
+    }
+}
+ 
+class escriuHolaAdeu {
+
+    boolean escritHola = false;
+
+    public synchronized void eAdeu() {
+        if (escritHola == false) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        escritHola = false;
+        System.out.println(" Adéu ");
+    }
+
+    public synchronized void eHola() {
+        System.out.println(" Hola ");
+        escritHola = true;
+        notify();
+    }
+    
+}
